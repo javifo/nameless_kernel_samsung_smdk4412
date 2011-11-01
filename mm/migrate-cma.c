acfccab5d0b9082a1baca3d9c2781030777c8302
Minchan Kim <minchan.kim@gmail.com>
2011-10-31 17:06:57 -0700

    mm: cma: migration: clean up unmap_and_move()
    commit 0dabec93de633a87adfbbe1d800a4c56cd19d73b upstream.
    
    Stable note: Not tracked in Bugzilla. This patch makes later patches
    	easier to apply but has no other impact.
    
    unmap_and_move() is one a big messy function.  Clean it up.
    
    Change-Id: I10500f156c408454aeb957774368669377fbe0c8
    Signed-off-by: Minchan Kim <minchan.kim@gmail.com>
    Reviewed-by: KOSAKI Motohiro <kosaki.motohiro@jp.fujitsu.com>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: KAMEZAWA Hiroyuki <kamezawa.hiroyu@jp.fujitsu.com>
    Cc: Mel Gorman <mgorman@suse.de>
    Cc: Rik van Riel <riel@redhat.com>
    Cc: Michal Hocko <mhocko@suse.cz>
    Cc: Andrea Arcangeli <aarcange@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Mel Gorman <mgorman@suse.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Javi Ferrer <javi.f.o@gmail.com>

--- mm/migrate-cma.c
+++ mm/migrate-cma.c
@@ -720,35 +720,14 @@ static int move_to_new_page(struct page *newpage, struct page *page,
 	return rc;
 }
 
-/*
- * Obtain the lock on page, remove all ptes and migrate the page
- * to the newly allocated page in newpage.
- */
-static int unmap_and_move(new_page_t get_new_page, unsigned long private,
-			struct page *page, int force, bool offlining, bool sync,
-			int pass, int tries)
+static int __unmap_and_move(struct page *page, struct page *newpage,
+				int force, bool offlining, bool sync, int pass, int tries)
 {
-	int rc = 0;
-	int *result = NULL;
-	struct page *newpage = get_new_page(page, private, &result);
+	int rc = -EAGAIN;
 	int remap_swapcache = 1;
 	int charge = 0;
 	struct mem_cgroup *mem;
 	struct anon_vma *anon_vma = NULL;
-
-	if (!newpage)
-		return -ENOMEM;
-
-	if (page_count(page) == 1) {
-		/* page was freed from under us. So we are done. */
-		goto move_newpage;
-	}
-	if (unlikely(PageTransHuge(page)))
-		if (unlikely(split_huge_page(page)))
-			goto move_newpage;
-
-	/* prepare cgroup just returns 0 or -ENOMEM */
-	rc = -EAGAIN;
 
 	if (!trylock_page(page)) {
 		if (!force || !sync) {
@@ -756,7 +735,7 @@ static int unmap_and_move(new_page_t get_new_page, unsigned long private,
 				printk("%s[%d] 1 ", __func__, __LINE__);
 				dump_page(page);
 			}
-			goto move_newpage;
+			goto out;
 		}
 
 		/*
@@ -777,7 +756,7 @@ static int unmap_and_move(new_page_t get_new_page, unsigned long private,
 				printk("%s[%d] 2 ", __func__, __LINE__);
 				dump_page(page);
 			}
-			goto move_newpage;
+			goto out;
 		}
 
 		lock_page(page);
@@ -916,8 +895,35 @@ uncharge:
 		mem_cgroup_end_migration(mem, page, newpage, rc == 0);
 unlock:
 	unlock_page(page);
+out:
+	return rc;
+}
 
-move_newpage:
+/*
+ * Obtain the lock on page, remove all ptes and migrate the page
+ * to the newly allocated page in newpage.
+ */
+static int unmap_and_move(new_page_t get_new_page, unsigned long private,
+			struct page *page, int force, bool offlining, bool sync, int pass, int tries)
+{
+	int rc = 0;
+	int *result = NULL;
+	struct page *newpage = get_new_page(page, private, &result);
+
+	if (!newpage)
+		return -ENOMEM;
+
+	if (page_count(page) == 1) {
+		/* page was freed from under us. So we are done. */
+		goto out;
+	}
+
+	if (unlikely(PageTransHuge(page)))
+		if (unlikely(split_huge_page(page)))
+			goto out;
+
+	rc = __unmap_and_move(page, newpage, force, offlining, sync, pass, tries);
+out:
 	if (rc != -EAGAIN) {
 		/*
 		 * A page that has been migrated has all references
