9a78cb856b8a62e194872a5aee8eea28b8d7f353
Javi Ferrer <javi.f.o@gmail.com>
2015-09-13 15:26:23 +0200

    cma: fix Merge commit 'b09b34258046c4555e535a279e29032303a932f8' into HEAD
    kernel-common 3.0.40
    
    Change-Id: I92e59a6fb77efaf86abcfa57b366a403ab14ee19

--- mm/compaction-cma.c
+++ mm/compaction-cma.c
@@ -227,6 +227,7 @@ isolate_migratepages_range(struct zone *zone, struct compact_control *cc,
 	unsigned long last_pageblock_nr = 0, pageblock_nr;
 	unsigned long nr_scanned = 0, nr_isolated = 0;
 	struct list_head *migratelist = &cc->migratepages;
+	isolate_mode_t mode = ISOLATE_ACTIVE|ISOLATE_INACTIVE;
 
 	/*
 	 * Ensure that there are not too many pages isolated from the LRU
@@ -302,8 +303,11 @@ isolate_migratepages_range(struct zone *zone, struct compact_control *cc,
 			continue;
 		}
 
+		if (!cc->sync)
+			mode |= ISOLATE_ASYNC_MIGRATE;
+
 		/* Try isolate the page */
-		if (__isolate_lru_page(page, ISOLATE_BOTH, 0) != 0)
+		if (__isolate_lru_page(page, mode, 0) != 0)
 			continue;
 
 		VM_BUG_ON(PageTransCompound(page));
@@ -659,7 +663,7 @@ static int compact_zone(struct zone *zone, struct compact_control *cc)
 		nr_migrate = cc->nr_migratepages;
 		err = migrate_pages(&cc->migratepages, compaction_alloc,
 				(unsigned long)cc, false,
-				cc->sync, 0);
+				cc->sync ? MIGRATE_SYNC_LIGHT : MIGRATE_ASYNC, 0);
 		update_nr_listpages(cc);
 		nr_remaining = cc->nr_migratepages;
 
